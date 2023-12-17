c1e41e44c26374b06f41c6a76ce747f8fe95e98e
AdrianOnn <129394367+AdrianOnn@users.noreply.github.com>
2023-12-18 06:09:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # Importing the necessary Python modules.
 import streamlit as st
+import pandas as pd
+import numpy as np
+from pomegranate import BayesianNetwork, DiscreteDistribution, State
 
 # Import necessary functions from web_functions
-from web_functions import load_data, train_model, predict
+from web_functions import load_data
 
 # Import pages
 from Tabs import home, data, predict_page, visualise
 
 # Configure the app
 st.set_page_config(
-    page_title='Stress Level Detector',
+    page_title='Student Stress Detector',
     page_icon='heavy_exclamation_mark',
     layout='wide',
     initial_sidebar_state='auto'
@@ -32,9 +35,21 @@ page = st.sidebar.radio("Pages", list(Tabs.keys()))
 # Loading the dataset.
 df, X, y = load_data()
 
+# Train model function using Pomegranate
+def train_model(X, y):
+    model = BayesianNetwork.from_samples(X.values, state_names=X.columns)
+    model.fit(X.values, y.values)
+    return model
+
+# Prediction function using Pomegranate
+def predict(model, features):
+    prediction = model.predict(features)
+    return prediction
+
 # Call the app function of the selected page to run
 if page == "Prediction":
-    Tabs[page].app(df, X, y, train_model, predict)
+    trained_model = train_model(X, y)
+    Tabs[page].app(df, trained_model, predict)
 elif page == "Visualisation":
     Tabs[page].app(df)
 elif page == "Data Info":
