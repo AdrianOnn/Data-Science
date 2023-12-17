f5936c6232df648495e36e43e5ae467882d13f9b
AdrianOnn <129394367+AdrianOnn@users.noreply.github.com>
2023-12-18 05:19:08 +0800

    Create home.py

---
+++ Tabs/home.py
@@ -0,0 +1,13 @@
+"""This modules contains data about home page"""
+
+# Import necessary modules
+import streamlit as st
+
+def app():
+    """This function create the home page"""
+    
+    # Add title to the home page
+    st.title("Stress Level Detector")
+
+    # Add image to the home page
+    st.image("./images/home.png")
