c34b954b4c459148c3f4e658311c2ed7de95b725
AdrianOnn <129394367+AdrianOnn@users.noreply.github.com>
2023-12-18 05:12:05 +0800

    Create web_functions.py

---
+++ web_functions.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from pgmpy.estimators import BayesianEstimator
+from pgmpy.models import BayesianNetwork
+import streamlit as st
+
+@st.cache()
+def load_data():
+    """This function returns the preprocessed data"""
+    df = pd.read_csv('Stress.csv')
+    df.rename(columns={"t": "bt"}, inplace=True)
+    X = df[["sr", "rr", "bt", "lm", "bo", "rem", "sh", "hr"]]
+    y = df['sl']
+    return df, X, y
+
+@st.cache()
+def train_model(X, y):
+    """This function trains the Bayesian Network model and returns the model and its structure"""
+    model = BayesianNetwork.from_samples(X, estimator=BayesianEstimator)
+    model.fit(X, y)
+    return model
+
+def predict(model, features):
+    prediction = model.predict(features)
+    return prediction
