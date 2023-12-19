87e01b3641746de95cbf94f1b78889117bce5d48
AdrianOnn <129394367+AdrianOnn@users.noreply.github.com>
2023-12-19 20:48:01 +0800

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -5,7 +5,7 @@ def app():
     """This function create the home page"""
     
     # Add title to the home page
-    st.title("Stress Level Detector")
+    st.title("Student Stress Detector")
 
     # Add image to the home page
     st.image("./Images/home.png")
