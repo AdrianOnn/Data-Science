cd7329e6f72b1d223003042d800f020bb65e2e7a
AdrianOnn <129394367+AdrianOnn@users.noreply.github.com>
2023-12-27 00:39:45 +0800

    Update web_functions.py

--- web_functions.py
+++ web_functions.py
@@ -1,32 +1,48 @@
+"""This module contains necessary function needed"""
+
+# Import necessary modules
 import numpy as np
 import pandas as pd
-from pgmpy.models import BayesianModel
-from pgmpy.estimators import BayesianEstimator
+from sklearn.tree import DecisionTreeClassifier
 import streamlit as st
+
 
 @st.cache()
 def load_data():
-# Load only necessary columns and convert data types
-    df = pd.read_csv('Stress.csv', usecols=['sr', 'rr', 'bt', 'lm', 'bo', 'rem', 'sh', 'hr', 'sl'])
-    for col in df.columns:
-        if df[col].dtype == 'object':
-            df[col] = pd.to_numeric(df[col], errors='coerce')
-    df['sl'] = df['sl'].astype('category')
+    """This function returns the preprocessed data"""
 
-    # Feature and target split
-    X = df.drop('sl', axis=1)  # Avoid creating a copy using iloc
+    # Load the Diabetes dataset into DataFrame.
+    df = pd.read_csv('Stress.csv')
+
+    # Perform feature and target split
+    X = df[["sr","rr","bt","lm","bo","rem","sh","hr"]]
     y = df['sl']
 
     return df, X, y
 
 @st.cache()
-def train_model(df):
-    model = BayesianModel([('sr', 'sl'), ('rr', 'sl'), ('bt', 'sl'), ('lm', 'sl'),
-                           ('bo', 'sl'), ('rem', 'sl'), ('sh', 'sl'), ('hr', 'sl')])
-    model.fit(data=df, estimator=BayesianEstimator, prior_type="BDeu")
-    return model
+def train_model(X, y):
+    """This function trains the model and return the model and model score"""
+    # Create the model
+    model = DecisionTreeClassifier(
+            ccp_alpha=0.0, class_weight=None, criterion='entropy',
+            max_depth=4, max_features=None, max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_samples_leaf=1, 
+            min_samples_split=2, min_weight_fraction_leaf=0.0,
+            random_state=42, splitter='best'
+        )
+    # Fit the data on model
+    model.fit(X, y)
+    # Get the model score
+    score = model.score(X, y)
 
-def predict(model, features):
-    prediction = model.predict_probability(features)
-    predicted_stress_level = np.argmax(prediction)
-    return predicted_stress_level, prediction
+    # Return the values
+    return model, score
+
+def predict(X, y, features):
+    # Get model and model score
+    model, score = train_model(X, y)
+    # Predict the value
+    prediction = model.predict(np.array(features).reshape(1, -1))
+
+    return prediction, score
