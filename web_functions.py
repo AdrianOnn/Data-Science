73520490c7dd339a568c7cff9982a866c79ced4b
AdrianOnn <129394367+AdrianOnn@users.noreply.github.com>
2023-12-26 22:13:54 +0800

    Update web_functions.py

--- web_functions.py
+++ web_functions.py
@@ -3,9 +3,9 @@
 # Import necessary modules
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
 import streamlit as st
-
+from pgmpy.models import BayesianModel
+from pgmpy.estimators import MaximumLikelihoodEstimator
 
 @st.cache()
 def load_data():
@@ -27,27 +27,23 @@ def load_data():
 
 @st.cache()
 def train_model(X, y):
-    """This function trains the model and return the model and model score"""
+    """This function trains the model and returns the model and model score"""
     # Create the model
-    model = DecisionTreeClassifier(
-            ccp_alpha=0.0, class_weight=None, criterion='entropy',
-            max_depth=4, max_features=None, max_leaf_nodes=None,
-            min_impurity_decrease=0.0, min_samples_leaf=1, 
-            min_samples_split=2, min_weight_fraction_leaf=0.0,
-            random_state=42, splitter='best'
-        )
-    # Fit the data on model
-    model.fit(X, y)
+    model = BayesianModel()
+    
+    # Estimate the parameters using Maximum Likelihood Estimation
+    estimator = MaximumLikelihoodEstimator(model, X)
+    model.fit(X, estimator=estimator)
+    
     # Get the model score
-    score = model.score(X, y)
-
+    score = model.score(X)
+    
     # Return the values
     return model, score
 
+@st.cache()
 def predict(X, y, features):
-    # Get model and model score
     model, score = train_model(X, y)
-    # Predict the value
     prediction = model.predict(np.array(features).reshape(1, -1))
 
     return prediction, score
